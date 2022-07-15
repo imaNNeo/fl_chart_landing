d769b9ea561c23e0cbf907c19bfcbe9cebe85870
Iman khoshabi <iman.neofight@gmail.com>
2022-07-16 03:01:53 +0430

    Bump version

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8c7f30030c28af8b531637e0ee0a1f00",
-"index.html": "b40520afab12419023da4b189db57858",
-"/": "b40520afab12419023da4b189db57858",
+"index.html": "ebeb6684825e303a0db80b60c7abdd4e",
+"/": "ebeb6684825e303a0db80b60c7abdd4e",
 "main.dart.js": "7119a3eff4069be50528cdd1cda4d002",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "bda8fcad0d1597bbbb50521c36f161f7",
